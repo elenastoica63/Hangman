9b8885e0151f8f13c2bad06a5a9519ac31b2fd47
Elena Stoica <ellennaa63@gmail.com>
2020-09-22 23:47:04 +0300

    Hello message, pick difficulty, choose word

--- play.py
+++ play.py
@@ -1,34 +1,70 @@
+import random
+import time
+
+word_list = ['Kabul', 'Albania', 'Tirana', 'Algeria', 'Algiers', 'Andorra', 'Angola', 'Luanda', 'Armenia', 'Yerevan', 'Austria', 'Vienna', 'Baku', 'Bahamas', 'Nassau', 'Bahrain', 'Manama', 'Dhaka', 'Belarus', 'Minsk', 'Belgium', 'Belize', 'Bhutan', 'Thimphu', 'Bolivia', 'La Paz', 'Brazil', 'Brunei', 'Sofia', 'Burundi', 'Praia', 'Yaounde', 'Canada', 'Ottawa', 'Bangui', 'Chile', 'China', 'Beijing', 'Colombia', 'Bogota', 'Comoros', 'Moroni', 'San Jose', 'Croatia', 'Zagreb', 'Cuba', 'Havana', 'Cyprus', 'Nicosia', 'Prague', 'Denmark', 'Roseau', 'Ecuador', 'Quito', 'Egypt', 'Cairo', 'Malabo', 'Eritrea', 'Asmara', 'Estonia', 'Tallinn', 'Fiji', 'Suva', 'Finland', 'France', 'Paris', 'Gabon', 'Gambia', 'Banjul', 'Georgia', 'Tbilisi', 'Germany', 'Berlin', 'Ghana', 'Accra', 'Greece', 'Athens', 'Guinea', 'Conakry', 'Bissau', 'Guyana', 'Hungary', 'Iceland', 'India', 'Jakarta', 'Iran', 'Tehran', 'Iraq', 'Baghdad', 'Ireland', 'Dublin', 'Israel', 'Italy', 'Rome', 'Jamaica', 'Japan', 'Tokyo', 'Jordan', 'Amman', 'Astana', 'Kenya', 'Nairobi', 'Kosovo', 'Kuwait', 'Bishkek', 'Laos', 'Latvia', 'Riga', 'Lebanon', 'Beirut', 'Lesotho', 'Maseru', 'Liberia', 'Libya', 'Tripoli', 'Vaduz', 'Vilnius', 'Skopje', 'Malawi', 'Male', 'Mali', 'Bamako', 'Malta', 'Mexico', 'Palikir', 'Moldova', 'Monaco', 'Monaco', 'Morocco', 'Rabat', 'Maputo', 'Namibia', 'Nauru', 'Nepal', 'Managua', 'Niger', 'Niamey', 'Nigeria', 'Abuja', 'Norway', 'Oslo', 'Oman', 'Muscat', 'Palau', 'Panama', 'Peru', 'Lima', 'Manila', 'Poland', 'Warsaw', 'Lisbon', 'Qatar', 'Doha', 'Romania', 'Russia', 'Moscow', 'Rwanda', 'Kigali', 'Samoa', 'Apia', 'Riyadh', 'Senegal', 'Dakar', 'Serbia', 'Honiara', 'Somalia', 'Seoul', 'Juba', 'Spain', 'Madrid', 'Sudan', 'Mbabane', 'Sweden', 'Bern', 'Syria', 'Taiwan', 'Taipei', 'Dodoma', 'Bangkok', 'Togo', 'Lome', 'Tunisia', 'Tunis', 'Turkey', 'Ankara', 'Tuvalu', 'Uganda', 'Kampala', 'Ukraine', 'Kyiv', 'London', 'Uruguay', 'Vanuatu', 'Caracas', 'Vietnam', 'Hanoi', 'Zambia', 'Lusaka', 'Harare']
+word_list2 = ['Argentina', 'Australia', 'Canberra', 'Azerbaijan', 'Bangladesh', 'Barbados', 'Bridgetown', 'Brussels', 'Belmopan', 'Sarajevo', 'Botswana', 'Gaborone', 'Brasilia', 'Bulgaria', 'Bujumbura', 'Cabo Verde', 'Cambodia', 'Phnom Penh', 'Cameroon', 'Santiago', 'Kinshasa', 'Costa Rica', 'Copenhagen', 'Dominica', 'El Salvador', 'Ethiopia', 'Addis Ababa', 'Helsinki', 'Libreville', 'Guatemala', 'Georgetown', 'Honduras', 'Budapest', 'Reykjavik', 'New Delhi', 'Indonesia', 'Jerusalem', 'Kingston', 'Kazakhstan', 'Kiribati', 'Pristina', 'Kuwait City', 'Kyrgyzstan', 'Vientiane', 'Monrovia', 'Lithuania', 'Luxembourg', 'Luxembourg', 'Macedonia', 'Madagascar', 'Lilongwe', 'Malaysia', 'Maldives', 'Valletta', 'Majuro', 'Mauritania', 'Nouakchott', 'Mauritius', 'Port Louis', 'Mexico City', 'Micronesia', 'Chisinau', 'Mongolia', 'Montenegro', 'Podgorica', 'Mozambique', 'Windhoek', 'Kathmandu', 'Amsterdam', 'New Zealand', 'Wellington', 'Nicaragua', 'North Korea', 'Pyongyang', 'Pakistan', 'Islamabad', 'Ngerulmud', 'Palestine', 'Ramallah', 'Panama City', 'Paraguay', 'Asunción', 'Portugal', 'Bucharest', 'Basseterre', 'Saint Lucia', 'Castries', 'Kingstown', 'San Marino', 'San Marino', 'Belgrade', 'Seychelles', 'Victoria', 'Freetown', 'Singapore', 'Singapore', 'Slovakia', 'Bratislava', 'Slovenia', 'Ljubljana', 'Mogadishu', 'Pretoria', 'South Korea', 'South Sudan', 'Khartoum', 'Suriname', 'Paramaribo', 'Swaziland', 'Stockholm', 'Damascus', 'Tajikistan', 'Dushanbe', 'Tanzania', 'Thailand', 'Ashgabat', 'Funafuti', 'Abu Dhabi', 'Washington', 'Montevideo', 'Uzbekistan', 'Tashkent', 'Port Vila', 'Venezuela', 'Zimbabwe']
+word_list3 = ['Afghanistan', 'Andorra la Vella', 'Buenos Aires', 'Bosnia and Herzegovina', 'Bandar Seri Begawan', 'Burkina Faso', 'Ouagadougou', 'Central African Republic', 'Democratic Republic of the Congo', 'Republic of the Congo', 'Brazzaville', 'Czech Republic', 'Dominican Republic', 'Santo Domingo', 'San Salvador', 'Equatorial Guinea', 'Guatemala City', 'Guinea-Bissau', 'Tegucigalpa', 'South Tarawa', 'Liechtenstein', 'Antananarivo', 'Kuala Lumpur', 'Marshall Islands', 'Ulaanbaatar', 'Yaren District', 'Netherlands', 'Papua New Guinea', 'Port Moresby', 'Philippines', 'Saint Kitts and Nevis', 'Saint Vincent and the Grenadines', 'Saudi Arabia', 'Sierra Leone', 'Solomon Islands', 'South Africa', 'Switzerland', 'Trinidad and Tobago', 'Port of Spain', 'Turkmenistan', 'United Arab Emirates', 'United Kingdom', 'United States of America']
+
+
 def get_hello_message():
     name = input("Hello, What is your name? ")
     return input("Well, " + name.capitalize() + ", Let's play Hangman!")
 
 print(get_hello_message())
 
-import random
-import countries_and_capitals_easy
-import countries_and_capitals_medium
-import countries_and_capitals_hard
+time.sleep(1)
 
-def difficulties():
-    difficulty = input("Choose a level - easy (maximum 7 letters and 7 lives), medium (Between 8-10 letters and 6 lives) or hard (11 letters or more and 5 lives)")
-    while True:
-        if difficulty == "easy":
-            secretWord = random.choice(countries_and_capitals_easy)
-            break
-        elif difficulty == "medium"
-            secretWord = random.choice(countries_and_capitals_medium)
-            break
+def pick_difficulty():
+    prompt = "Pick a difficulty, please. (Easy, Medium, Hard)\n>"
+    choice = ""
+    while choice not in ["easy", "medium", "hard"]:
+        choice = input(prompt)
+        choice = choice.lower()
+
+print(pick_difficulty())
+
+
+def choose_word(choice):
+    if choice == "easy":
+        secretWord = random.choice(word_list)
+    elif choice == "medium":
+        secretWord = random.choice(word_list1)
+    elif choice == "hard":
+        secretWord = random.choice(word_list2)
+
+
+def play(word, lives):
+    word = list(word)
+    lives = list(lives)
+    blanks = "_" * len(word)
+    blanks = list(blanks)
+    guessed = []
+    incorrect = 7
+    while incorrect > 0:
+        print("\n" + gallows[incorrect]
+              + "\nYou have {} chances left.".format(incorrect)
+              + "\nYour word: " + "".join(blanks)
+              + "\nGuessed letters: " + ", ".join(guessed)
+             )
+        letter = input("Your guess: ").lower()
+        if len(letter) == 1 and letter.isalpha():
+            if letter in guessed:
+                print("\n\nYou already guessed that!")
+            elif letter in word:
+                for index,character in enumerate(word):
+                    blanks = list(blanks)
+                    if character == letter:
+                        blanks[index] = letter
+                        current = "".join(blanks)
+                        if blanks == word:
+                            print("\n\nCONGRATULATIONS, YOU WON!!\nYour word was " + ''.join(word) + ".\n")
+                            play_again()
+            elif letter not in word:
+                incorrect -= 1
+                guessed.append(letter)
         else:
-            secretWord = random.choice(countries_and_capitals_hard)
-            break
-
-def printGuessedLetter():
-    attempts = (len(secretWord) + 2)
-    print("Your Secret word is: " + ''.join(difficulties())
-    for n in secretWord:
-        difficulties.append('_')
-    printGuessedLetter()
-
-print("The number of allowed guesses for this word is:", attempts)
-
-print(difficulties())
+            print("\n\n!Only single letters allowed!\n\n")
+    else:
+        print(gallows[0])
+        print("\nSorry " + player + ", your game is over!\nYour word was " + ''.join(word) + ".")
+        play_again()
